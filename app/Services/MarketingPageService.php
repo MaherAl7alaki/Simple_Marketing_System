1cdd95db1c18ab013bdab08229105a152d266970
MaherAl7alaki <tarkhean@gmail.com>
2025-11-15 23:58:10 +0300

    add function create marketinpage and include cloudinary

--- app/Services/MarketingPageService.php
+++ app/Services/MarketingPageService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Http\Resources\MarketingPageResource;
 use App\Models\MarketingPage;
+use Cloudinary\Cloudinary;
 
 class MarketingPageService
 {
@@ -40,6 +41,28 @@ class MarketingPageService
     public function createMarketingPage($request)
     {
 
+
+        $tmpPath = tempnam(sys_get_temp_dir(), 'cert_') . '.png';
+        file_put_contents($tmpPath, $request->image);
+
+        $cloudinary = new Cloudinary();
+
+        $uploaded = $cloudinary->uploadApi()->upload($tmpPath, [
+            'folder' => 'Marketing_System/Marketing_Page/images',
+            'resource_type' => 'image',
+        ]);
+        unlink($tmpPath);
+
+        $marketingPage = MarketingPage::create([
+            $request->validated(),
+            'image' => $uploaded['secure_url'] ?? null,
+        ]);
+
+        return [
+          'data' => new MarketingPageResource($marketingPage),
+            'message' => "Create Marketing page successfully",
+            'status' => 201
+        ];
     }
 
 }
